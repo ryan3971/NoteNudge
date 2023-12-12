1bca6253c2bdac8306fc06e07b6174043d0041e9
ryan3971 <tyrrellr@mcmaster.ca>
2023-12-11 21:30:28 -0500

    Tray implemented

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const { app, BrowserWindow, Tray, Menu, ipcMain } = require('electron');
 const path = require('node:path')
 
 let mainWindow 
+let tray = null
 
 function createWindow () {
   mainWindow = new BrowserWindow({
@@ -17,6 +18,44 @@ function createWindow () {
 
   mainWindow.loadFile('index.html')
 }
+
+function createTray() {
+  const trayIcon = path.join(__dirname, 'public/images/icon.png');
+  tray = new Tray(trayIcon);
+
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Open',
+      click: () => {
+        if (!mainWindow) {
+          createWindow();
+        }
+        mainWindow.show();
+      },
+    },
+    {
+      label: 'Quit',
+      click: () => {
+        app.isQuitting = true;
+        app.exit(); // All windows will be closed immediately without asking the user, and the before-quit and will-quit events will not be emitted
+      },
+    },
+  ]);
+
+  tray.setToolTip('My Electron App');
+  tray.setContextMenu(contextMenu);
+
+  // Show the window when the tray icon is clicked
+  // tray.on('click', () => {
+  //   if (!mainWindow) {
+  //     createMainWindow();
+  //   }
+  //   mainWindow.show();
+  // });
+}
+
+
+
 // Function to set the timer to reopen the application
 function setReopenTimer() {
   
@@ -25,7 +64,9 @@ function setReopenTimer() {
 
   // Create the timer
   reopenTimer = setTimeout(() => {
-    createWindow();
+    if (!mainWindow) {
+      createWindow();
+    }
   }, reopenDelay);
 }
 
@@ -33,12 +74,17 @@ function setReopenTimer() {
 app.on('ready', () => {
   console.log(`ready`);
   createWindow();
-
+  createTray()
+ 
   // called when the window is closing
   app.on('before-quit', (event) => {
     console.log(`before-quit`);
-    // Override the default behavior of closing the window
     event.preventDefault()
+
+    // set mainWindow to null so that the application doesn't try to reopen it
+    mainWindow = null;
+
+    // Override the default behavior of closing the window
     setReopenTimer()
   });
 });
@@ -49,6 +95,5 @@ ipcMain.on('close-application', (event) => {
   console.log(`Button clicked in the main process!`);
 
   // Handle closing the application here
-  mainWindow.close();
-  setReopenTimer();
+  mainWindow.close(); // Try to close the window. This has the same effect as a user manually clicking the close button of the window
 });
