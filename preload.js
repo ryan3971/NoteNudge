53e51089cce570a3256aa0f830351937e3b5282e
ryan3971 <tyrrellr@mcmaster.ca>
2023-12-24 14:38:18 -0500

    Working implementation of settings

--- preload.js
+++ preload.js
@@ -1,7 +1,5 @@
 const { contextBridge, ipcRenderer } = require('electron/renderer')
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  handleCloseApplication: () => ipcRenderer.send('close-application'),
-  handleSubmitEntry: (entry) => ipcRenderer.send('submit-entry', entry),
   handleOpenSettings: () => ipcRenderer.send('open-settings')
 });
--- renderer.js
+++ renderer.js
@@ -1,23 +1,5 @@
 // // renderer.js
-const close_button = document.getElementById('btn_close');
-
-const entryInput = document.getElementById('entry_input')
-const submitEntryButton = document.getElementById('submit_entry_button')
 const settingsButton = document.getElementById('open_settings')
-
-// Create a listener for the close-application event
-close_button.addEventListener('click', () => {
-  console.log(`Button clicked!`);
-
-  // Handle closing the application here
-  window.electronAPI.handleCloseApplication('close-application')
-});
-
-// Create a listener for the submit-entry event
-submitEntryButton.addEventListener('click', () => {
-  const entry = entryInput.value;
-  window.electronAPI.handleSubmitEntry(entry);
-});
 
 // Create a listener for the open-settings event
 settingsButton.addEventListener('click', () => {
